56c5df1c2659074e95490947b2f838afc7d95ace
kinjo yuji <kinjoyuji@gmail.com>
2017-12-07 01:46:40 +0900

    マテリアルUIのサンプルを表示

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import NavBar from './NavBar';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      open: false
+    }
+  }
+  handleToggle() {
+    this.setState({
+      open: !this.state.open
+    })
+  }
   render() {
     return (
       <div>
-        HelloWorld
+        <NavBar
+          onToggle={() => this.handleToggle()}
+          open={this.state.open}
+        />
       </div>
     );
   }
