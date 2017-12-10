c74b2c9aa47f34c08870564b5ff51502dfa0bae6
kinjo yuji <kinjoyuji@gmail.com>
2017-12-10 12:33:23 +0900

    APIコール実装をdomeinに移譲

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,8 @@ import React from 'react';
 import GeocodeResult from './GeocodeResult';
 import SearchForm from './SearchForm';
 import Map from './Map';
-
-import axios from 'axios';
-import  PropTypes from 'prop-types';
+import { geocode } from '../domain/Geocoder'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 export default class App extends React.Component {
   constructor(props){
@@ -23,7 +19,7 @@ export default class App extends React.Component {
   }
 
   setErrorMessage(message) {
-    this.this.setState({
+    this.setState({
       address: message,
       location: {
         lat: 0,
@@ -33,32 +29,22 @@ export default class App extends React.Component {
   }
 
   handlePlaceSubmit(place){
-    axios
-      .get(GEOCODE_ENDPOINT, { params: {address: place} })
-      .then((results) => {
-        const data = results.data;
-        const result = data.results[0];
-        switch (data.status) {
-          case 'OK': {
-            const location = result.geometry.location;
-            this.setState({
-              address: result.formatted_address,
-              location: location,
-            });
+    geocode(place)
+      .then(({ status, address, location }) => {
+        switch (status) {
+          case 'OK':{
+            this.setState({ address, location});
             break;
           }
           case 'ZERO_RESULTS': {
-            this.setErrorMessage('エラーが起きました.');
+            this.setErrorMessage('結果が見つかりませんでした');
             break;
           }
-          default: {
-            this.setErrorMessage('エラーが起きました.');
+          default:{
+            this.setErrorMessage('エラーが発生しました');
           }
         }
       })
-      .catch((error) => {
-        this.setErrorMessage('通信に失敗しました.');
-      });
   }
 
   render() {
