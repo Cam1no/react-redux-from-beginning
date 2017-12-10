fc503f943d2652e51aa074f9300b6dbb17dc4623
kinjo yuji <kinjoyuji@gmail.com>
2017-12-10 11:52:23 +0900

    google map APIを叩いて、任意の位置情報をMapで表示

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,59 @@ import GeocodeResult from './GeocodeResult';
 import SearchForm from './SearchForm';
 import Map from './Map';
 
+import axios from 'axios';
 import  PropTypes from 'prop-types';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 export default class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      place: '',
+      place: '東京タワー',
       lat: 35.658581,
       lng: 139.745433,
     }
   }
 
+  setErrorMessage(message) {
+    this.this.setState({
+      address: message,
+      lat: 0,
+      lng: 0,
+    })
+  }
+
+
   handlePlaceSubmit(place){
-    this.setState({place})
+    axios
+      .get(GEOCODE_ENDPOINT, { params: {address: place} })
+      .then((results) => {
+        const data = results.data;
+        const result = data.results[0];
+        switch (data.status) {
+          case 'OK': {
+            const location = result.geometry.location;
+            this.setState({
+              address: result.formatted_address,
+              lat: location.lat,
+              lng: location.lng,
+            });
+            break;
+          }
+          case 'ZERO_RESULTS': {
+            this.setErrorMessage('エラーが起きました.');
+            break;
+          }
+          default: {
+            this.setErrorMessage('エラーが起きました.');
+          }
+        }
+      })
+      .catch((error) => {
+        this.setErrorMessage('通信に失敗しました.');
+      });
   }
 
   render() {
