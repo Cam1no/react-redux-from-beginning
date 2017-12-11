31a5e8e18d6236dedcb9ffd9ec7e1b985cba00f3
kinjo yuji <kinjoyuji@gmail.com>
2017-12-11 17:35:17 +0900

    検索コンポーネントをSearchPageに切り出し

--- src/components/App.js
+++ src/components/App.js
@@ -1,95 +1,24 @@
 import React from 'react';
-import GeocodeResult from './GeocodeResult';
-import SearchForm from './SearchForm';
-import Map from './Map';
-import { geocode } from '../domain/Geocoder'
-import { searchHotelByLocation } from '../domain/HotelRepository'
+import SearchPage from './SearchPage';
 import Grid from 'material-ui/Grid';
-import HotelsTable from './HotelsTable'
-import _ from 'lodash';
-
-const sortedHotels = (hotels, sortKey) => _.sortBy(hotels, h => h[sortKey]);
 
 export default class App extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      place: '東京タワー',
-      address: '',
-      location: {
-        lat: 35.658581,
-        lng: 139.745433,
-      },
-      hotels: [
-        { id: 12, name: 'ホテルオークラ', url: 'http://google.com', minPrice: 0, thumbnail: '', reviewAverage: 1, reviewCount: 1, distance: 100},
-        { id: 1212, name: 'アパホテル', url: 'http://google.com', minPrice: 0, thumbnail: '', reviewAverage: 1, reviewCount: 1, distance: 100},
-        { id: 1243, name: 'ロワジールホテル', url: 'http://google.com', minPrice: 0, thumbnail: '', reviewAverage: 1, reviewCount: 1, distance: 100},
-      ],
-      sortKey: 'minPrice',
-    }
-  }
-
-  setErrorMessage(message) {
-    this.setState({
-      address: message,
-      location: {
-        lat: 0,
-        lng: 0,
-      },
-    })
-  }
-
-  handlePlaceSubmit(place){
-    geocode(place)
-      .then(({ status, address, location }) => {
-        switch (status) {
-          case 'OK':{
-            this.setState({ address, location});
-            return searchHotelByLocation(location);
-          }
-          case 'ZERO_RESULTS': {
-            this.setErrorMessage('結果が見つかりませんでした');
-            break;
-          }
-          default:{
-            this.setErrorMessage('エラーが発生しました');
-          }
-        }
-        return [];
-      })
-      .then((hotels) => {
-        this.setState({ hotels: sortedHotels(hotels, this.state.sortKey) })
-      })
-  }
-  handleSortKeyChange(sortKey){
-    console.log(sortKey);
-    this.setState({ sortKey, hotels: sortedHotels(this.state.hotels, sortKey) })
-  }
-
   render() {
     return (
-      <div style={ { textAlign: 'center'} }>
-        <h1>緯度経度検索</h1>
-        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)}/>
-        <GeocodeResult
-          address={this.state.address}
-          location={this.state.location}
-        />
-        <div style={{width: '1100px', margin: 'auto'}}>
-          <Grid container>
-            <Grid item xs={6}>
-              <Map location={ this.state.location }/>
-            </Grid>
-            <Grid item xs={6}>
-              <h2>ホテル検索結果</h2>
-              <HotelsTable
-                hotels={this.state.hotels}
-                sortKey={this.state.sortKey}
-                onSort={(sortKey) => this.handleSortKeyChange(sortKey)}
-              />
-            </Grid>
+      <div>
+        <Grid container>
+          <Grid item xs={3}>
+            <div style={{ width: '60%', 'background-color': '#dcdddf', height: '1000px', padding: '50px' }}>
+              <ul style={{ 'list-style': 'none' }}>
+                <li><a href="#">Top</a></li>
+                <li><a href="#">About</a></li>
+              </ul>
+            </div>
           </Grid>
-        </div>
+          <Grid item xs={9}>
+            <SearchPage />
+          </Grid>
+        </Grid>
       </div>
     );
   }
---
+++ src/components/SearchPage.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import GeocodeResult from './GeocodeResult';
+import SearchForm from './SearchForm';
+import Map from './Map';
+import { geocode } from '../domain/Geocoder'
+import { searchHotelByLocation } from '../domain/HotelRepository'
+import Grid from 'material-ui/Grid';
+import HotelsTable from './HotelsTable'
+import _ from 'lodash';
+
+const sortedHotels = (hotels, sortKey) => _.sortBy(hotels, h => h[sortKey]);
+
+export default class SearchPage extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      place: '東京タワー',
+      address: '',
+      location: {
+        lat: 35.658581,
+        lng: 139.745433,
+      },
+      hotels: [
+        { id: 12, name: 'ホテルオークラ', url: 'http://google.com', minPrice: 0, thumbnail: '', reviewAverage: 1, reviewCount: 1, distance: 100},
+        { id: 1212, name: 'アパホテル', url: 'http://google.com', minPrice: 0, thumbnail: '', reviewAverage: 1, reviewCount: 1, distance: 100},
+        { id: 1243, name: 'ロワジールホテル', url: 'http://google.com', minPrice: 0, thumbnail: '', reviewAverage: 1, reviewCount: 1, distance: 100},
+      ],
+      sortKey: 'minPrice',
+    }
+  }
+
+  setErrorMessage(message) {
+    this.setState({
+      address: message,
+      location: {
+        lat: 0,
+        lng: 0,
+      },
+    })
+  }
+
+  handlePlaceSubmit(place){
+    geocode(place)
+      .then(({ status, address, location }) => {
+        switch (status) {
+          case 'OK':{
+            this.setState({ address, location});
+            return searchHotelByLocation(location);
+          }
+          case 'ZERO_RESULTS': {
+            this.setErrorMessage('結果が見つかりませんでした');
+            break;
+          }
+          default:{
+            this.setErrorMessage('エラーが発生しました');
+          }
+        }
+        return [];
+      })
+      .then((hotels) => {
+        this.setState({ hotels: sortedHotels(hotels, this.state.sortKey) })
+      })
+  }
+  handleSortKeyChange(sortKey){
+    console.log(sortKey);
+    this.setState({ sortKey, hotels: sortedHotels(this.state.hotels, sortKey) })
+  }
+
+  render() {
+    return (
+      <div style={ { textAlign: 'center'} }>
+        <h1>ホテル検索</h1>
+        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)}/>
+        <GeocodeResult
+          address={this.state.address}
+          location={this.state.location}
+        />
+        <div style={{width: '100%', margin: 'auto'}}>
+          <Grid container>
+            <Grid item xs={6}>
+              <Map location={ this.state.location }/>
+            </Grid>
+            <Grid item xs={6}>
+              <h2>ホテル検索結果</h2>
+              <HotelsTable
+                hotels={this.state.hotels}
+                sortKey={this.state.sortKey}
+                onSort={(sortKey) => this.handleSortKeyChange(sortKey)}
+              />
+            </Grid>
+          </Grid>
+        </div>
+      </div>
+    );
+  }
+}
