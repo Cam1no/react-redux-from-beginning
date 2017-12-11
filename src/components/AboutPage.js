8c6a338b4a16e3b8f082795d4073ffe5be6fcac3
kinjo yuji <kinjoyuji@gmail.com>
2017-12-11 17:55:29 +0900

    react-routerを使って、aboutページとsearchページを設置

---
+++ src/components/AboutPage.js
@@ -0,0 +1,10 @@
+import React, {PropTypes} from 'react';
+
+export const AboutPage = (props) => {
+  return (<div>AboutPage</div>);
+}
+
+export default AboutPage;
+
+AboutPage.propTypes = {
+};
--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import SearchPage from './SearchPage';
+import AboutPage from './AboutPage';
 import Grid from 'material-ui/Grid';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <Grid container>
-          <Grid item xs={3}>
-            <div style={{ width: '60%', 'background-color': '#dcdddf', height: '1000px', padding: '50px' }}>
-              <ul style={{ 'list-style': 'none' }}>
-                <li><a href="#">Top</a></li>
-                <li><a href="#">About</a></li>
-              </ul>
-            </div>
+      <Router>
+        <div>
+          <Grid container>
+            <Grid item xs={3}>
+              <div style={{ width: '60%', 'background-color': '#dcdddf', height: '1000px', padding: '50px' }}>
+                <ul style={{ 'list-style': 'none' }}>
+                  <li><Link to="/">Top</Link></li>
+                  <li><Link to="/about">About</Link></li>
+                </ul>
+              </div>
+            </Grid>
+            <Grid item xs={9}>
+              <Switch>
+                <Route exact path='/' component={SearchPage}></Route>
+                <Route exact path='/about' component={AboutPage}></Route>
+              </Switch>
+            </Grid>
           </Grid>
-          <Grid item xs={9}>
-            <SearchPage />
-          </Grid>
-        </Grid>
-      </div>
+        </div>
+      </Router>
     );
   }
 }
